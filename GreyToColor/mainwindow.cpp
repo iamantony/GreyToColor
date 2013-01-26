8f4bc03b76402eb49d1e97b4317e7c66fde1b2fb
Antony Cherepanov <antony.cherepanov@gmail.com>
2013-01-27 01:14:12 +0600

    Licence added to some files
    main.cpp
    programstatus.h
    mainwindow.h
    mainwindow.cpp
    statusbar.h
    statusbar.cpp

--- GreyToColor/mainwindow.cpp
+++ GreyToColor/mainwindow.cpp
@@ -1,3 +1,21 @@
+/* === This file is part of GreyToColor ===
+ *
+ *	Copyright 2012, Antony Cherepanov <antony.cherepanov@gmail.com>
+ *
+ *	GreyToColor is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	GreyToColor is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with GreyToColor. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
